6522a314c19c08e4409917d4d4b580a8951f2b5d
andrii_h <galgayand@gmail.com>
2024-04-01 02:28:13 +0300

    Update vite.config.ts

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+  optimizeDeps: {
+    include: ['@mui/material/Tooltip', '@emotion/styled', '@mui/material/Unstable_Grid2'],
+  },
     server: {
+      port: 3000,
         proxy: {
             '/api': {
                 target: 'http://localhost:3000/',
