9202b198bbd33911eb78265a56dcd6b77a86269a
andrii_h <galgayand@gmail.com>
2024-04-17 01:54:44 +0300

    update store

--- apps/client/src/store/ui/ui_slice.ts
+++ apps/client/src/store/ui/ui_slice.ts
@@ -1,24 +1,34 @@
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 interface IUIState {
+  menuDrawer: boolean
   historyDrawer: boolean
-  cardForm:boolean
+  cardForm: boolean
 }
+//
+// interface IPayload {
+//   type: 'menuDrawer | historyDrawer | cardForm'
+//   drawerState: boolean
+// }
 
 const initialState: IUIState = {
+  menuDrawer: false,
   historyDrawer: false,
-  cardForm:false
+  cardForm: false
 }
 
 const uiSlice = createSlice({
   name: 'ui',
   initialState,
   reducers: {
-    toggleHistory: (state,{payload}:PayloadAction<boolean>) => {
+    toggleHistory: (state, {payload}: PayloadAction<boolean>) => {
       state.historyDrawer = payload
     },
-    toggleCardForm: (state,{payload}:PayloadAction<boolean>) => {
+    toggleCardForm: (state, {payload}: PayloadAction<boolean>) => {
       state.cardForm = payload
+    },
+    toggleMenu: (state, {payload}: PayloadAction<boolean>) => {
+      state.menuDrawer = payload
     },
 
   }
